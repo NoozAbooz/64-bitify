86abe78d38d3d177df43af19ccbef99026656659
mobilegmYT <44128563+mobilegmYT@users.noreply.github.com>
2020-09-22 15:25:36 -0600

    e

--- 64-bitify.sh
+++ 64-bitify.sh
@@ -1,6 +1,6 @@
 title="64-bitify by Raspberry Pi News"
 prompt="Pick an option:"
-options=("Install 64-bit kernal code (do this first!)" "Enable 64-bit kernal" "Disable 64-bit kernal" "Revert config.txt")
+options=("Install 64-bit kernal code (do this first!)" "Enable 64-bit kernal" "Disable 64-bit kernal" "Revert config.txt" "Update script")
 
 echo "$title"
 echo ""
@@ -13,15 +13,15 @@ select opt in "${options[@]}" "Quit to shell"; do
 
     case "$REPLY" in
 
-    1 ) sudo cp /boot/config.txt /boot/config.txt.bak && sudo awk '/max_framebuffers=2/{print $0 RS "arm_64bit=1";next}1' /boot/config.txt > tmp && mv tmp /boot/config.txt && echo "64-bit kernal code is now installed. You can check if you have 64 bit by typing 'uname -a'. If it have aarch64 in the output, then you are on 64-bit mode.";;
-    2 ) sudo perl -p -i -e 's/#arm_64bit=1/arm_64bit=1/g' /boot/config.txt && echo "64-bit is now enabled";;
-    3 ) sudo perl -p -i -e 's/arm_64bit=1/#arm_64bit=1/g' /boot/config.txt && echo "64-bit is now disabled";;
-    4 ) sudo mv /boot/config.txt.bak /boot/config.txt && echo "Reverted config.txt to when 64-bit was installed";;
-    
+    1 ) sudo rpi-update -y&& sudo cp /boot/config.txt /boot/config.txt.bak && sudo awk '/max_framebuffers=2/{print $0 RS "arm_64bit=1";next}1' /boot/config.txt > tmp && mv tmp /boot/config.txt && echo "64-bit kernal code is now installed. You can check if you have 64 bit by typing 'uname -a'. If it have aarch64 in the output, then you are on 64-bit mode." && read -p "The system will now restart. When ready, press [Enter] to continue or [Ctrl-C] to cancel..." && sudo reboot;;
+    2 ) sudo perl -p -i -e 's/#arm_64bit=1/arm_64bit=1/g' /boot/config.txt && echo "64-bit is now enabled" && read -p "The system will now restart. When ready, press [Enter] to continue or [Ctrl-C] to cancel..." && sudo reboot;;
+    3 ) sudo perl -p -i -e 's/arm_64bit=1/#arm_64bit=1/g' /boot/config.txt && echo "64-bit is now disabled" && read -p "The system will now restart. When ready, press [Enter] to continue or [Ctrl-C] to cancel..." && sudo reboot;;
+    4 ) sudo mv /boot/config.txt.bak /boot/config.txt && echo "Reverted config.txt to when 64-bit was installed" && read -p "The system will now restart. When ready, press [Enter] to continue or [Ctrl-C] to cancel..." && sudo reboot;;
+    5 ) cd ~ && wget https://raw.githubusercontent.com/mobilegmYT/64-bitify/master/64-bitify.sh;;
+	
     $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
 
     esac
 
 done
-
--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # 64-bitify
-Script to make your raspberry pi 64-bit. Works on all Raspberry Pi 4s running Raspberry Pi OS and Raspberry Pi OS-based OSes. This is alpha code and might break your pi sp MAKE SURE TO BACKUP!
+Script to make your raspberry pi 64-bit. Works on all Raspberry Pi 4s running Raspberry Pi OS and Raspberry Pi OS-based OSes. This is alpha code and might brick your pi so MAKE SURE TO BACKUP! The code simply makes your kernal 64-bit, so you get a performance boost, while sill letting you run your 32-bit apps in a 32-bit userland. 
 
-Download script to make your existing install 64-bit: `wget https://raw.githubusercontent.com/mobilegmYT/64-bitify/master/64-bitify.sh & chmod +x 64-bitify.sh & ./64-bitify.sh`
+#Caveats 
+There are caveats to this, but if you have any problems, then try disabling 64-bit with the script and reboot. If it still is broken, then maybe use that backup I told you to make. So far, it's been noted that Kodi runs a little slower because the renderer is blocked, Java (NOT openJDK) doesnt work,  KDE shell doesnt work either, and Box86 doesnt work without recompiling for 64 bit. Wolfaram and Mathamatica, doesnt work, but a workaround is listed here:https://www.raspberrypi.org/forums/viewtopic.php?t=250730.
 
+#Download script
+Download script to make your existing install 64-bit: `cd ~ && wget https://raw.githubusercontent.com/mobilegmYT/64-bitify/master/64-bitify.sh & chmod +x 64-bitify.sh & ./64-bitify.sh`
+
+#Download OS
 Download 64-bit img version of TwisterOS: http://download1980.mediafire.com/0d6s18sgqkcg/dbc83jpil9fj54e/TwisterOS-64bit.img.xz
 
-Help: Go to the Pi Labs discord https://discord.gg/QGVRzJ for help with this version of TwisterOS. My nametag is "Raspberry Pi News#7199".
+#Performance
+The pi isn't noticably faster, but if you do a high-performance task like 
 
-Credits: The code to make your pi 64-bit is from https://medium.com/for-linux-users/how-to-make-your-raspberry-pi-4-faster-with-a-64-bit-kernel-77028c47d653
+#Help
+Go to the Pi Labs discord https://discord.gg/QGVRzJ for help with this version of TwisterOS. My nametag is "Raspberry Pi News#7199".
+
+#Credits
+The code to make your pi 64-bit is from https://medium.com/for-linux-users/how-to-make-your-raspberry-pi-4-faster-with-a-64-bit-kernel-77028c47d653
 
