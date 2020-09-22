30407d6061901ec5d74107d4f3be05b7f2267da4
mobilegmYT <44128563+mobilegmYT@users.noreply.github.com>
2020-09-22 13:33:58 -0600

    Create 64-bitify.sh

---
+++ 64-bitify.sh
@@ -0,0 +1,23 @@
+title="64-bitify by Raspberry Pi News"
+echo "This script will make your raspberry pi kernal 64-bit while allowing your 32-bit applications to still run in a 32-bit userland. This is alpha code, so MAKE SURE YOU BACKUP BEFOREHAND, or else you could break your install."
+prompt="Pick an option:"
+options=("Install 64-bit kernal code (do this first!)" "Enable 64-bit kernal" "Disable 64-bit kernal" "Update script" "Revert config.txt")
+
+echo "$title"
+PS3="$prompt "
+select opt in "${options[@]}" "Quit"; do 
+
+    case "$REPLY" in
+
+    1 ) sudo cp /boot/config.txt /boot/config.txt.bak && sudo awk '/max_framebuffers=2/{print $0 RS "arm_64bit=1";next}1' /boot/config.txt > tmp && mv tmp /boot/config.txt && echo "64-bit kernal code is now installed. You can check if you have 64 bit by typing 'uname -a'. If it have aarch64 in the output, then you are on 64-bit mode.";;
+    2 ) sudo perl -p -i -e 's/#arm_64bit=1/arm_64bit=1/g' /boot/config.txt && echo "64-bit is now enabled";;
+    3 ) sudo perl -p -i -e 's/arm_64bit=1/#arm_64bit=1/g' /boot/config.txt && echo "64-bit is now disabled";;
+    4 ) 
+    5 ) sudo mv /boot/config.txt.bak /boot/config.txt && echo "Reverted config.txt to when 64-bit was installed"
+
+    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one.";continue;;
+
+    esac
+
+done
